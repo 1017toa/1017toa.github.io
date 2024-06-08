77df6a3571a869dac12a7508d362fac3f9d34113
정한샘 <1017toa@naver.com>
2024-06-08 18:57:18 +0900

    Updates

--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'jhsaem15@gmail.com',
-    phone: '+821039377684',
-    address: '서울시 구로구 천왕동',
-
-    sheetAPI: ''
-}
--- src/data/headerData.js
+++
@@ -1,9 +0,0 @@
-import resume from '../assets/pdf/resume.pdf'
-
-export const headerData = {
-    name: '정한샘',
-    title: "백엔드 엔지니어",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
-    resumePdf: resume
-}
